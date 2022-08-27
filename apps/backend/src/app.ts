beca5726672ae6d0e81dd2c8c1f84fd45757d957
Rayahhhmed <raiyan.ahmed@unsw.edu.au>
2022-08-27 17:57:22 +1000

    db collections work

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,26 +1,18 @@
 import express, {Request, Response} from 'express';
-import mongoose from 'mongoose';
-
+import { connectToDB, db } from './db';
 
 const app = express();
 
-mongoose.connect('mongodb+srv://csesoc:syncs@cluster0.3ih5idr.mongodb.net/?retryWrites=true&w=majority'
-  ).then(() => {
-    app.listen(3001);
-    console.log("sussy things happening at port 3000");
-  }).catch(err => console.log('err', err))
-
-
-/**
- * Routes
- */
-
 app.get("/course/:id", (req: Request, res: Response) => { 
   const courseId = req.params.id;
-  return res.send("amongus code for " + courseId);
+  console.log(courseId)
+  
+  db.courses?.collection(courseId).find().toArray((err, result) => res.json(result));
 })
 
-app.get("/courses/", (req: Request, res: Response) => { 
-  const courses = mongoose.connection.db.listCollections().toArray((_, names) => names)
-  return res.send(courses);
+app.get("/courses", (req: Request, res: Response) => { 
+  return res.send({"courses": db.coursesList});
 })
+
+connectToDB().then(() => {
+app.listen(3001)});
