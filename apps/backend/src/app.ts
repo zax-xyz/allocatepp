2aceee06c63ab84b994b6f4d3f7d778bc3090c6e
Rayahhhmed <raiyan.ahmed@unsw.edu.au>
2022-08-27 15:46:07 +1000

    Created Course get endpoint

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,12 +1,16 @@
 
 import express, {Request, Response} from 'express';
-
+import mongoose from 'mongoose';
 const app = express();
 
-app.listen(3000, () => {
-  console.log("sussy things happening at port 3000");
-});
+mongoose.connect('mongodb+srv://csesoc:syncs@cluster0.3ih5idr.mongodb.net/?retryWrites=true&w=majority'
+  ).then(result => {
+    app.listen(3000);
+    console.log("sussy things happening at port 3000");
+  }).catch(err => console.log('err', err))
 
-app.get("/course/", (req: Request, res: Response) => { 
-  return res.send("amongus");
+
+app.get("/course/:id", (req: Request, res: Response) => { 
+  const courseId = req.params.id;
+  return res.send("amongus code for " + courseId);
 })
